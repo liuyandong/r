98cf4570d443a91d64c2191c44f3a3e8aa4f8296
liuyandong <liuyandong@gmail.com>
2020-02-18 18:19:19 +0800

    show avatar

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'jquery-rails',            '4.3.1'
 gem 'turbolinks',              '5.0.1'
 gem 'jbuilder',                '2.7.0'
 gem 'rucaptcha'
+gem 'ruby_identicon'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,16 @@ module UsersHelper
 
   # Returns the Gravatar for the given user.
   def gravatar_for(user, options = { size: 80 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    
     size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    
+    if user.email.downcase == "liuyandong@gmail.com"
+      image_tag(asset_path('dong.png'), alt: user.name, class: "gravatar", size: "#{size}")
+    else
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      base64_identicon = RubyIdenticon.create_base64(gravatar_id)
+      image_tag("data:image/png;base64,#{base64_identicon}", alt: user.name, class: "gravatar", size: "#{size}")
+    end
+    
   end
 end
