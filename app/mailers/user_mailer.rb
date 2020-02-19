bcc88debfeed9a00cafde24fe682b7456321aeb8
liuyandong <liuyandong@gmail.com>
2020-02-19 12:15:44 +0800

    email bug fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: “yandong.liu@qq.com”, subject: "#{user.name} | #{user.email} 的激活邮件"
+    mail to: 'yandong.liu@qq.com', subject: "Account activation"
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "密码重置"
+    mail to: user.email, subject: "Password reset"
   end
 end
