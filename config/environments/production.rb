93f539b2aacae66de047a0f982a7b5eaa3d718b3
liuyandong <liuyandong@gmail.com>
2020-02-19 10:54:06 +0800

    email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,16 +63,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'polar-inlet-49278.herokuapp.com'
+  #host = 'polar-inlet-49278.herokuapp.com'
+  host = 'lmzdx.net'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    :user_name      => 'lvmaozidaxue@gmail.com',
+    :password       => 'kljdxrrqpngzpigd',
+    #:domain         => 'heroku.com',
+    #:enable_starttls_auto => true
   }
   
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
