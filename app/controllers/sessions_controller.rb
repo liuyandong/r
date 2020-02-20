ee7535fefebd90129a6c0bd1a5605c148b77b76b
liuyandong <liuyandong@gmail.com>
2020-02-20 16:21:57 +0800

    登录后重定向到首页

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
       if user.activated?
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
+        #redirect_back_or user
+        redirect_to root_url
       else
         message  = "Account not activated. "
         message += "Check your email for the activation link."
